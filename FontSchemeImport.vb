9c5c11a1cea7af2f55bc264881332ab9f63fdd0d
Chris Laughlin <cjlaughlin@gmail.com>
2016-02-03 07:29:49 -0800

    added fontSchemeImport
    This script is used to create font themes in mac ppt

---
+++ FontSchemeImport.vb
@@ -0,0 +1,35 @@
+Sub fontThemeImport()
+
+Dim fileAccessGranted As Boolean
+Dim filePermissionCandidates
+
+'this is a user-created file on the desktop, created by using the 'save command' on line 34
+'assumes user has already edited the xml file'
+fontScheme = "/Users/Chris/Desktop/PPTFontScheme.xml"
+'prompt the user for a name to save the new theme as'
+newScheme = InputBox("Enter a scheme name")
+
+'fontlink is a symlink for:
+'Library/Group\ Containers/UBF8T346G9.Office/User\ Content.localized/Themes.localized/Theme\ Fonts
+'for some reason, this path needs to be found by using terminal and tabbing to get the space escapes and the .localized stuff
+'the important note is that by default, ppt searches for files starting from /Users/Chris/Library/Containers/com.microsoft.Powerpoint/Data
+'this folder contains several symlinks, which, if used, can allow write access to the whole system
+'new symlinks are created in terminal with: ln -s /any/file/on/the/disk symlink-name
+
+newSchemeDest = "fontlink/" & newScheme & ".xml"
+
+'Create an array with file paths for which permissions are needed
+'each file accessed only needs permission ONCE
+filePermissionCandidates = Array(fontScheme)
+
+'Request Access from User_
+fileAccessGranted = GrantAccessToMultipleFiles(filePermissionCandidates)
+
+'returns true if access granted, false otherwise
+
+'load the scheme
+ActivePresentation.Designs(1).SlideMaster.Theme.ThemeFontScheme.Load fontScheme
+'move the theme for the location for user font schemes'
+ActivePresentation.Designs(1).SlideMaster.Theme.ThemeFontScheme.Save newSchemeDest
+
+End Sub
